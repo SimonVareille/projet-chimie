9552558d0eaee399e80971bc4d9cde46d73dab98
Simon Vareille <si.vareille@gmail.com>
2019-03-04 17:08:08 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -34,13 +34,16 @@ else:
 from data_reader import DataReader
 
 import kivy
-kivy.require('1.0.7')
+kivy.require('1.10.1')
+
+from kivy.config import Config
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, StringProperty, BooleanProperty
 from kivy.clock import Clock
 
 
@@ -48,34 +51,62 @@ class MainWindow(Widget):
     '''Classe représentant la fenêtre principale
     Elle a le même nom que dans app.kv.
     '''
+    curveBoxLayout = ObjectProperty(None)
+    expCurveCheckBox = ObjectProperty(None)
+    expCurveText = StringProperty("[color=ff3399]Courbe\n experimentale[/color]")
+    
+    thCurveCheckBoxActive = BooleanProperty(True)
     def __init__(self, **kwargs):
         super(MainWindow, self).__init__(**kwargs)
         
+        
         self.t = cottrel.create_t(0, 20, 1000)
         self.I = cottrel.courbe_cottrel_th(1, 1, 10**-3, 10**(-5), self.t)
+        
+        
+        self.mainGraph = maingraph.MainGraph(self.t, self.I)
         
         reader = DataReader("experimental.csv")
         
         self.expt = reader.get_t()
         self.expI = reader.get_I()
         
-        self.mainGraph = maingraph.MainGraph(self.t, self.I)
         
         self.mainGraph.set_experimental_data(self.expt, self.expI)
         self.mainGraph.display_experimental()
+        
         self.mainGraph.set_limit_interval()
         
-        linreg = LinearRegression(self.expt, self.expI)
-        
-        linreg.set_t_interval(1, 50)
-        
-        expD, coeff = linreg.regression(1, 1, 10**-3)
-        
-        self.mainGraph.set_expD(expD)
+        if config.USE_NUMPY:
+            linreg = LinearRegression(self.expt, self.expI)
+            
+            linreg.set_t_interval(1, 50)
+            
+            expD, coeff = linreg.regression(1, 1, 10**-3)
+            
+            self.mainGraph.set_expD(expD)
         
         self.mainGraph.update()
         
-        self.ids.curveBoxLayout.add_widget(self.mainGraph.get_canvas())
+        self.curveBoxLayout.add_widget(self.mainGraph.get_canvas())
+    
+    def on_expCurveCheckBox_active(self, active):
+        if active:
+            self.mainGraph.display_experimental()
+            self.mainGraph.set_limit_interval()
+        else:
+            self.mainGraph.display_experimental(False)
+            self.mainGraph.set_limit_interval()
+        self.mainGraph.update()
+    
+    def on_thCurveCheckBox_active(self, active):
+        if active:
+            self.mainGraph.display_theoric()
+            self.mainGraph.set_limit_interval()
+        else:
+            self.mainGraph.display_theoric(False)
+            self.mainGraph.set_limit_interval()
+        self.mainGraph.update()
 
 
 class AppApp(App):
