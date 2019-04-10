ac4421585e35f6a28010d1d9faa1eb946dcd3558
SebastienPlancq <46991813+SebastienPlancq@users.noreply.github.com>
2019-04-10 09:13:50 +0200

    résiste aux valeurs négatives de Dth
    Les valeurs négatives faisaient planter car on en fait une sqrt pour la reg linéaire

--- main.py
+++ main.py
@@ -255,7 +255,10 @@ Veuillez les enlever avec le bouton[/color] [color=000000]«Sélectionner l'inte
     def update_values(self,instance,text):
         '''Met à jour la courbe principale avec les nouvelles valeurs.
         '''
-        self.valDth=self.buttonDth.value
+        if self.buttonDth.value >=0:
+            self.valDth=self.buttonDth.value
+        else :
+            ErrorPopup(text="Dth ne peut pas prendre une valeur négative !\nLa valeur de Dth utilisée pour le graphique est inchangée").open()
         self.valN=self.buttonN.value
         self.valC=self.buttonC.value
         self.valS=self.buttonS.value
